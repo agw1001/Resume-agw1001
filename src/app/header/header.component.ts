b6bbc593cbf9d2a86259fd9ad6a3b37574040ede
adam <agw1001@sru.edu>
2019-11-15 11:07:54 -0500

    Added img and formatted header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title = "Adam Weaver"
+  Picture = "\assets\\img\\IMG_20191115_103820.jpg"
   address = "774 Sunset Circle Cranberry Township, PA 16066"
   email = "weaveradam1191@gmail.com"
   phone = "(724)-831-9209"
